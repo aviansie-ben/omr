050b392b8e553a4636a4b975a932bd6250505b4d
Benjamin Thomas <ben@benthomas.ca>
2019-09-24 03:13:14 +0000

    Remove all unsupported opcode tests
    Previously, the test compiler test suite included a number of tests
    designed to ensure that certain opcodes were unimplemented. These tests
    repeatedly caused spurious test failures as some of these opcodes were
    implemented in the various codegens, so they have been removed.
    
    Signed-off-by: Ben Thomas <ben@benthomas.ca>

--- fvtest/compilertest/tests/PPCOpCodesTest.hpp
+++ fvtest/compilertest/tests/PPCOpCodesTest.hpp
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2017 IBM Corp. and others
+ * Copyright (c) 2000, 2019 IBM Corp. and others
  *
  * This program and the accompanying materials are made available under
  * the terms of the Eclipse Public License 2.0 which accompanies this
@@ -39,7 +39,6 @@ class PPCOpCodesTest : public OpCodesTest
    virtual void invokeCompareTests();
    virtual void invokeBitwiseTests();
    virtual void invokeAddressTests();
-   virtual void UnsupportedOpCodesTests();
 
    virtual void compileDisabledConvertTestMethods();
    virtual void compileDisabledCompareTestMethods();
--- fvtest/compilertest/tests/X86OpCodesTest.hpp
+++ fvtest/compilertest/tests/X86OpCodesTest.hpp
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2017 IBM Corp. and others
+ * Copyright (c) 2000, 2019 IBM Corp. and others
  *
  * This program and the accompanying materials are made available under
  * the terms of the Eclipse Public License 2.0 which accompanies this
@@ -45,7 +45,6 @@ class X86OpCodesTest : public OpCodesTest
    virtual void invokeCompareTests();
    virtual void invokeTernaryTests();
    virtual void invokeAddressTests();
-   virtual void UnsupportedOpCodesTests();
 
    virtual void compileDisabledIntegerArithmeticTestMethods();
    virtual void invokeDisabledIntegerArithmeticTests();
